2f1fdff51658094c0bf36d36b340c70106b1c36b
omkargundlur <omkargundlur@gmail.com>
2020-08-09 19:10:55 +0530

    next set

--- first.sh
+++ first.sh
@@ -7,4 +7,9 @@ wc first.sh
 #Print multiple Lines
 echo -e "Hello World \n Next Hello India"
 
+echo This will print red color
 echo -e "\e[31mHello"
+
+#This Will reset the color
+echo this will rest the color
+echo -e "\e[31mHello World\e[0m"
