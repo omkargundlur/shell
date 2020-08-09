96846aef07f5f4cb95114d18aeb255e48c966242
omkargundlur <omkargundlur@gmail.com>
2020-08-09 21:38:09 +0530

    a

--- inputarguments.sh
+++ inputarguments.sh
@@ -2,7 +2,7 @@
 echo "script name is $0"
 echo "First argument is $1"
 echo "Second argument is $2"
-echo "all arguments" = $*
+echo "all arguments = $*"
 echo "total Number of arguments is $#"
 
 #Executing script like ./inputarguments.sh arg1 arg2 #will give the number of arguments
