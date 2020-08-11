332c0e6b66e7e6eea0eeb585b3e8eb9fc52bf584
omkargundlur <omkargundlur@gmail.com>
2020-08-11 21:53:24 +0530

    a

--- project.sh
+++ project.sh
@@ -1,7 +1,18 @@
 ##!/bin/bash
-echo -e "Installing the software ${1} "
+#echo -e "Installing the software ${1} "
 <<'howtorun'
 [user@server ~]$ sh project.sh MSSQL
 Installing the software MSSQL
 [user@server ~]$
 howtorun
+
+FRONTEND(
+    {
+    echo "Install front end"
+
+}
+
+
+case $1 in 
+    frontend)
+        FRONTEND
